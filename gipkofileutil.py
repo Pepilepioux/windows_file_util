9714f15e7b1087eb55d6716adc4f8c88e135ac30
Pepilepioux <pepilepioux@ovh.fr>
2017-12-14 12:50:10 +0100

    Ajouté le traitement des variables d'environnement de windows dans la recherche du programme par défaut

--- gipkofileutil.py
+++ gipkofileutil.py
@@ -7,6 +7,8 @@
 """
 import win32com.client
 import winreg
+import re
+import os
 
 VERSION = '1.0'
 
@@ -23,6 +25,19 @@ def creerRaccourci(nom, cible)	:
         return excpt.with_traceback(tb)
 
     return ''
+
+
+# ------------------------------------------------------------------------------------
+def traduireVarEnvWindows(nomVar):
+    listeVars = [e for e in os.environ]
+    pat = re.compile('%.*?%', re.IGNORECASE)
+    res = re.findall(pat, nomVar)
+
+    for p in res:
+        if p.strip('%').upper() in [e.upper() for e in listeVars]:
+            nomVar = nomVar.replace(p, os.environ[p.strip('%')])
+
+    return nomVar
 
 
 # ------------------------------------------------------------------------------------
@@ -65,5 +80,10 @@ def chercherProgrammeDefaut(typeFichier):
 
         except:
             valeur = None
+
+    if valeur:
+        #   ACHTUNG ! certains chemins sont donnés avec des variables d'environnement du type "%VAR%",
+        #   que windows sait interpréter, mais pas Python !
+        valeur = traduireVarEnvWindows(valeur)
 
     return valeur
